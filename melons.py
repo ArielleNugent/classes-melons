57e120ef114a8c9be503b1911f997db88e9d8967
Arielle Nugent <ariellenugent@gmail.com>
2021-05-06 22:28:09 +0000

    Completed Part 2
    Coauthored by: Kavya Nagarajan <kavya.0219@gmail.com>

--- melons.py
+++ melons.py
@@ -4,19 +4,26 @@
 class AbstractMelonOrder():
     """An abstract base class that other melon orders inherit from."""
 
-    def __init__(self, species, qty):
+    def __init__(self, species, qty, tax, order_type):
 
         self.species = species
         self.qty = qty
         self.shipped = False
-        self.order_type = "domestic"
-        self.tax = 0.08
+        self.order_type = order_type
+        self.tax = tax
 
     def get_total(self):
         """Calculate price, including tax."""
-
-        base_price = 5
-        total = (1 + self.tax) * self.qty * base_price
+        
+        if (self.species).lower() == "christmas melons":
+            base_price = 7.5
+        else:
+            base_price = 5
+        
+        if self.qty < 10 and self.order_type == "international":
+            total = ((1 + self.tax) * self.qty * base_price) + 3
+        else:
+            total = (1 + self.tax) * self.qty * base_price 
 
         return total
 
@@ -27,18 +34,9 @@ class AbstractMelonOrder():
 
 class DomesticMelonOrder(AbstractMelonOrder):
     """A melon order within the USA."""
-
-    # def __init__(self, species, qty):
-    #     """Initialize melon order attributes."""
-
-    #     self.species = species
-    #     self.qty = qty
-    #     self.shipped = False
-    #     self.order_type = "domestic"
-    #     self.tax = 0.08
-
-   
-
+    def __init__(self, species, qty):
+        super().__init__(species, qty, 0.08, "domestic")
+        
 
 class InternationalMelonOrder(AbstractMelonOrder):
     """An international (non-US) melon order."""
@@ -46,14 +44,18 @@ class InternationalMelonOrder(AbstractMelonOrder):
     def __init__(self, species, qty, country_code):
         """Initialize melon order attributes."""
         
-        # self.species = species
-        # self.qty = qty
         self.country_code = country_code
-        # self.shipped = False
-        super().__init__(species, qty)
-        self.tax = 0.17
-        self.order_type = "international"
-
+        super().__init__(species, qty, 0.17, "international")
+        
+    # def get_total(self):
+    #     """Calculate price, including tax."""
+    #     super().get_total(species, qty)
+        
+    #     if self.qty < 10:
+    #         total = ((1 + self.tax) * self.qty * base_price) + 3
+    #     else:
+    #         total = (1 + self.tax) * self.qty * base_price
+    #     return total
 
     def get_country_code(self):
         """Return the country code."""
